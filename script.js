878d0e3cba6d6ff840e960d657b5b6c76acce59a
jchuajy <j.chua.jy@gmail.com>
2018-05-03 16:03:50 +0800

    done basic

--- script.js
+++ script.js
@@ -1,7 +1,71 @@
-var monkey = "hello";
+class Page extends React.Component {
+
+  render() {
+
+    return (
+        <div>
+        <Header userName="Kanye West" />
+        <Navbar />
+        <Twittercard allTweets={tweets} />
+        </div>
+    );
+  };
+};
+
+class Header extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+      <h1>{this.props.userName}</h1>
+      </div>
+    );
+  };
+};
+
+class Navbar extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+      <h2>This is my navbar</h2>
+      </div>
+    );
+  };
+};
+
+class Twittercard extends React.Component {
+
+  render() {
+
+    let tweetElement = this.props.allTweets.map(item => {
+      return (
+        <div>
+          <h3>{item.user.name}</h3>
+          <h4>@{item.user.screen_name}</h4>
+          <h5>{item.created_at}</h5>
+          <img src={item.user.profile_image_url} />
+          <h4>{item.text}</h4>
+          <h5>RT: {item.retweet_count}</h5>
+          <h5>FAV: {item.favorite_count}</h5>
+        </div>
+      )
+    });
+
+    return (
+      <div>
+        {tweetElement}
+      </div>
+    )
+  };
+};
+
+
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
-    document.getElementById('root')
+  <Page />,
+  document.getElementById('root')
 );
+
